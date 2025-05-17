4448d3a8aff2d9c829947b8b78e9abe446c9fe02
Sabbir-96 <sabbirislam9404@gmail.com>
2025-05-18 01:39:35 +0600

    update env file and firebase file

--- firebase.init.js
+++ firebase.init.js
@@ -1,20 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDl7NWtiY2glxV20jUuPUMyGcSC3AEBYo",
-  authDomain: "coffee-project-auth-fb3c5.firebaseapp.com",
-  projectId: "coffee-project-auth-fb3c5",
-  storageBucket: "coffee-project-auth-fb3c5.firebasestorage.app",
-  messagingSenderId: "82971133461",
-  appId: "1:82971133461:web:7e6ec2ee202cd338b4b01c"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
